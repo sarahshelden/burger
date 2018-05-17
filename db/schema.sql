7e8998ef971794fea1e47b63ecc243af045b0304
Sarah Shelden <sarah.shelden113@gmail.com>
2018-05-17 10:03:54 -0400

    it still doesnt work

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgers_db;
+
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(50),
+devoured BOOLEAN;
+PRIMARY KEY (id)
+);
+
+SELECT * from burgers;
+
+
+
---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO burger (burger_name, devored)
+VALUES("Hamburger", null);
+VALUES("Cheeseburger", null);
+VALUES ("Quinoa Burger", null);
+
+
+
